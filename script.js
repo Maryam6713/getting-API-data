9d99577a5cb1f8cb95e15c5a8e9cd50e88dc529b
Maryam6713 <maryammalik6713@gmail.com>
2024-09-18 18:19:16 +0500

    initial commit

--- script.js
+++ script.js
@@ -1,65 +1,53 @@
+//--------automatic scrolling section 
 function scrollToRecipes() {
-    const recipesSection = document.getElementById('recipes-section');
+    var recipesSection = document.getElementById('recipes-section');
     recipesSection.scrollIntoView({ behavior: 'smooth' });
 }
 //---------------changes images
-const images = [
+var images = [
     'food7.png',
     'food3.png',
     'food8.png',
     'food11.png',
     'food2.png'
 ];
+var currentImageIndex = 0;
 
-let currentImageIndex = 0;
-
-// Get the image element
+// -------------Get the image element
 const imageElement = document.getElementById('circle');
 
-// Add click event listener to the image
+//------------ Add click event listener to the image
 imageElement.addEventListener('click', function() {
-    // Increment the image index
-    currentImageIndex = (currentImageIndex + 1) % images.length;
 
-    // Update the image source to the next image in the array
+    currentImageIndex = (currentImageIndex + 1) % images.length;
     imageElement.src = images[currentImageIndex];
 });
 //---------------------getting API
-const containerRecipes = document.getElementById('recipes');
-//const containerProducts = document.getElementById('products-container');
+var containerRecipes = document.getElementById('recipes');
 
-// Function to fetch and display recipes
 function displayRecipes() {
     fetch('https://dummyjson.com/recipes')
         .then(response => response.json())
         .then(data => {
-            // Clear previous recipe results
-            containerRecipes.innerHTML = '';
-
-            // Assuming 'data.recipes' holds the array of recipes
-            data.recipes.forEach(item => {
-                const card = document.createElement('div');
-card.classList.add('col-lg-3', 'col-md-4', 'col-sm-6', 'mt-3');
-
-card.innerHTML = `
-    <div class="card text-center" style="width: 18rem;">
-        <img src="${item.image}" class="card-img card-img-top img-fluid" alt="${item.name}">
-        <div class="card-body">
-            <h5 class="card-title">${item.name}</h5>
-            <a href="#" class="btn btn-warning">Select</a>
-        </div>
-    </div>
-`;
-
-document.getElementById('recipes').appendChild(card);
-
-
-
-                containerRecipes.appendChild(card);
+            
+            (data.recipes || []).forEach(item => {
+                var card = `
+                    <div class="col-lg-3 col-md-4 col-sm-6 mt-3">
+                        <div class="card text-center" style="width: 18rem;">
+                            <img src="${item.image}" class="card-img card-img-top img-fluid" alt="${item.name}">
+                            <div class="card-body">
+                                <h5 class="card-title">${item.name}</h5>
+                                <a href="#" class="btn btn-warning">watch recipe</a>
+                            </div>
+                        </div>
+                    </div>`;
+                
+        
+                containerRecipes.innerHTML += card;
             });
         })
-        .catch(error => {
-            console.error('Error fetching recipes:', error);
-        });
+        .catch(error => console.error('Error fetching recipes:', error));
 }
-displayRecipes()
+
+
+displayRecipes();
